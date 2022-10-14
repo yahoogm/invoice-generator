ab565a6c738592469c928b6727983dd0a92ceb06
anditanz <pkoking24@gmail.com>
2022-10-14 10:56:28 +0700

    update github

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
+import {React} from 'react'
+import Header from './components/header/Header';
+import Bisa from './components/header/Bisa';
+import Susah from './components/header/Susah';
+import Time from './components/header/Time';
+import { InvoiceProvider } from './hooks/context';
+
 
 function App() {
-  return <div className="App"></div>;
+  return <InvoiceProvider>
+  <div className='container'>
+    <div className='flex flex-row'>
+      <div className='flex flex-col w-full'>
+        <Header />
+        <Bisa />
+        <Susah />
+      </div>
+      <Time />
+    </div>
+    </div>
+    
+  </InvoiceProvider>;
 }
 
 export default App;
