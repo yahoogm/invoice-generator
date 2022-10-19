3d75b4f79ef31c49abe1264cc687bde075406cc9
DavidSitompul <dpaulsitompul@gmail.com>
2022-10-19 15:07:02 +0700

    fix paid

--- src/components/footer/Total.jsx
+++ src/components/footer/Total.jsx
@@ -14,7 +14,7 @@ const Total = ({ inputtext, inputplace, value, onChange, style, button }) => {
               <input
                 style={{ borderWidth: 0, cursor: "pointer" }}
                 type="text"
-                className="mt-1 px-1  py-1  w-1/2 bg-white focus:outline-none focus:border-green-400 focus:ring-green-300   rounded-md sm:text-sm focus:ring-0 "
+                className="mt-1 px-1  py-1  w-full bg-white focus:outline-none focus:border-green-400 focus:ring-green-300   rounded-md sm:text-sm focus:ring-0 "
                 placeholder={inputtext}
               />
             </div>
