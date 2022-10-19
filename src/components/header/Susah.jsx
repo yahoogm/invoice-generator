d4aa5a3ffadc504d78f942e05ae7e0d5a6b53300
anditanz <pkoking24@gmail.com>
2022-10-19 15:00:49 +0700

    update github

--- src/components/header/Susah.jsx
+++ src/components/header/Susah.jsx
@@ -3,7 +3,7 @@ import Leaderbar from "./Leaderbar";
 const Susah =() => {
     return (
         <>
-        <div className="grid grid-cols-2 gap-0 w-full ml-5" >
+        <div className="grid grid-cols-2 gap-2 w-full " >
         <Leaderbar judul={"bill to"} title={"Who is ths invoice to?"}/>
         
         <Leaderbar  judul={"ship to"} title={"(optional)"}/>
