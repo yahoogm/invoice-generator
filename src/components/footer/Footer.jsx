fdf2844207e61a29fb33bb9679ff5e0f847d9c41
DavidSitompul <dpaulsitompul@gmail.com>
2022-10-19 14:21:08 +0700

    update style

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,57 +12,20 @@ const Footer = ({ subtotal }) => {
 
   return (
     <>
-      <div
-        style={{
-          width: "100%",
-          padding: "0 100px",
-        }}
-      >
-        <Sidebar title={"Notes"} place={"Notes - any relevant information not already covered"} />
-        <br />
-        <br />
+      <div className="flex justify-between ">
+        <div className="mt-2 mb-2">
+          <Sidebar title={"Notes"} place={"Notes - any relevant information not already covered"} />
+          <br />
+          <br />
+          <Sidebar title={"Terms"} place={"Term and Conditions - late fees,payment methods,delivery schedule"} />
+          {/*  */}
+        </div>
 
-        <Sidebar title={"Terms"} place={"Term and Conditions - late fees,payment methods,delivery schedule"} />
-        {/*  */}
-        <div
-          style={{
-            // float: "right",
-            marginTop: "-230px",
-            marginLeft: "50%",
-            // background: "blue",
-          }}
-        >
+        <div className="mt-2 mb-2">
           <Total inputtext={"Subtotal"} value={subtotal} style={{ borderWidth: 0, boxShadow: "none", textAlign: "right" }} />
-          <Total
-            inputtext={"Discount"}
-            onChange={(e) => setDiscount(e.target.value)}
-            value={discount}
-            button={
-              <>
-                <button>delete</button>
-              </>
-            }
-          />
-          <Total
-            inputtext={"Tax"}
-            onChange={(e) => setTax(e.target.value)}
-            value={tax}
-            button={
-              <>
-                <button>delete</button>
-              </>
-            }
-          />
-          <Total
-            inputtext={"shipping"}
-            onChange={(e) => setPayment(e.target.value)}
-            button={
-              <>
-                <button>delete</button>
-              </>
-            }
-            value={payment}
-          />
+          <Total inputtext={"Discount"} onChange={(e) => setDiscount(e.target.value)} value={discount} />
+          <Total inputtext={"Tax"} onChange={(e) => setTax(e.target.value)} value={tax} />
+          <Total inputtext={"shipping"} onChange={(e) => setPayment(e.target.value)} value={payment} />
 
           <Total inputtext={"Total"} value={total} style={{ borderWidth: 0, boxShadow: "none", textAlign: "right" }} />
 
