559e504df7bd6b19c8c394f43263b8374f11b599
yahoogm <yaho.garfield@gmail.com>
2022-10-24 15:33:25 +0700

    fixing print

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Content from "../src/components/content/Content"
-import Footer from "../src/components/footer/Footer"
-import Header from "../src/components/header/header"
-import Bisa from "../src/components/header/Bisa"
-import Susah from "../src/components/header/Susah"
-import Time from "../src/components/header/Time"
-import React, { useRef, useState } from "react"
-import { InvoiceProvider } from "./hooks/context"
-import ReactToPrint from "react-to-print"
+import Content from "../src/components/content/Content";
+import Footer from "../src/components/footer/Footer";
+import Header from "../src/components/header/header";
+import Bisa from "../src/components/header/Bisa";
+import Susah from "../src/components/header/Susah";
+import Time from "../src/components/header/Time";
+import React, { useRef, useState } from "react";
+import { InvoiceProvider } from "./hooks/context";
+import ReactToPrint from "react-to-print";
 
 const App = () => {
   const [items, setItems] = useState([
@@ -16,71 +16,70 @@ const App = () => {
       qty: 1,
       rate: 0,
     },
-  ])
-  const [btn, setBtn] = useState(true)
-  const [notes, setNotes] = useState("")
-  const [terms, setTerms] = useState("")
+  ]);
+  const [notes, setNotes] = useState("");
+  const [terms, setTerms] = useState("");
 
   // ---- Start Function to handle some new items ---- //
   const handleAdd = () => {
-    const id = items.length ? items[items.length - 1].id + 1 : 1
-    setItems([...items, { id: id, item: "", qty: 1, rate: 0 }])
-  }
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    setItems([...items, { id: id, item: "", qty: 1, rate: 0 }]);
+  };
 
   // ---- End Function to handle some new items ---- //
 
   // ---- Start Function to handle changes some  item ---- //
   const handleOnChangeItem = (index, item) => {
-    const newItem = [...items]
-    newItem[index].item = item
-    setItems(newItem)
-  }
+    const newItem = [...items];
+    newItem[index].item = item;
+    setItems(newItem);
+  };
   // ---- End Function to handle hanges some item ---- //
 
   // ---- Start Function to handle changes some  rate ---- //
   const handleOnChangeRate = (index, rate) => {
-    const newRate = [...items]
-    newRate[index].rate = rate
-    setItems(newRate)
-  }
+    const newRate = [...items];
+    newRate[index].rate = rate;
+    setItems(newRate);
+  };
   // ---- End Function to handle hanges some rate ---- //
 
   // ---- Start Function to handle delete some  item ---- //
   const handleDelete = (id) => {
-    const delItem = items.filter((item) => item.id !== id)
-    setItems(delItem)
-  }
+    const delItem = items.filter((item) => item.id !== id);
+    setItems(delItem);
+  };
   // ---- End Function to handle delete some  item ---- //
 
   // ---- Start Function to handle changes some  qty ---- //
   const handleOnChangeQty = (index, qty) => {
-    const newQty = [...items]
-    newQty[index].qty = qty
-    setItems(newQty)
-  }
+    const newQty = [...items];
+    newQty[index].qty = qty;
+    setItems(newQty);
+  };
   // ---- End Function to handle hanges some items ---- //
 
-  let subtotal = 0
+  let subtotal = 0;
   items.forEach((item) => {
-    subtotal += Number(item.qty) * Number(item.rate)
-  }, 0)
+    subtotal += Number(item.qty) * Number(item.rate);
+  }, 0);
 
-  const [discount, setDiscount] = useState(0)
-  const [payment, setPayment] = useState(0)
-  const [tax, setTax] = useState(0)
-  const [paid, setPaid] = useState(0)
-  let total = subtotal + Number(payment) - Number(discount) + Number(tax)
+  const [discount, setDiscount] = useState(0);
+  const [payment, setPayment] = useState(0);
+  const [tax, setTax] = useState(0);
+  const [paid, setPaid] = useState(0);
+  let total = subtotal + Number(payment) - Number(discount) + Number(tax);
 
   // ---- Start Function to format the amount to IDR ---- //
 
-  const toCurrency = (number, currency, lang = undefined) => Intl.NumberFormat(lang, { style: "currency", currency }).format(number)
+  const toCurrency = (number, currency, lang = undefined) => Intl.NumberFormat(lang, { style: "currency", currency }).format(number);
   // ---- End Function to format the amount to IDR ---- //
 
-  const componentRef = useRef()
+  const componentRef = useRef();
 
   return (
-    <div className="w-[900px] m-auto border-l border-t border-b border-r-2 shadow-lg relative space-y-5">
-      <ReactToPrint trigger={() => <button className="bg-[#009e90] p-4 right-0 absolute rounded-bl-3xl  hover:bg-[#009e74] border-[#10806f] border text-white shadow-md">Print</button>} content={() => componentRef.current} />
+    <div ref={componentRef} className="w-[900px] m-auto border-l border-t border-b border-r-2 shadow-lg relative space-y-5">
+      <ReactToPrint trigger={() => <button className="bg-[#009e90] p-4 right-0 absolute rounded-bl-3xl  hover:bg-[#009e74] border-[#10806f] border text-white shadow-md print:hidden">Print</button>} content={() => componentRef.current} />
       <div className=" space-y-5 p-4">
         <InvoiceProvider>
           <div className="flex flex-row justify-between">
@@ -115,70 +114,7 @@ const App = () => {
           </div>
         </InvoiceProvider>
       </div>
-      <button className="bg-[#009e90] p-4 w-full  hover:bg-[#009e74] border-[#10806f] border text-white shadow-lg" onClick={() => setBtn(!btn)}>
-        Show Invoice
-      </button>
-      {btn && (
-        <div ref={componentRef} className="p-2">
-          <table className="w-full table-fixed">
-            <thead className="bg-gray-800 text-white text-left">
-              <tr>
-                <th className="p-1 w-[35%]">Item</th>
-                <th className="p-1 w-[15%]">Quantity</th>
-                <th className="p-1 w-[25%]">Rate</th>
-                <th className="p-1 w-[25%]">Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-              {items.map((i) => {
-                return (
-                  <tr key={i.id}>
-                    <td className="font-semibold">{i.item}</td>
-                    <td>{i.qty}</td>
-                    <td>{toCurrency(i.rate, "IDR")}</td>
-                    <td>{toCurrency(i.qty * i.rate, "IDR")}</td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
-          <div className="flex justify-between mt-10 ml-10">
-            <div className="space-y-2">
-              <div>
-                <h5 className="text-gray-500 font-semibold">Notes :</h5>
-                <p>{notes}</p>
-              </div>
-              <div>
-                <h5 className="text-gray-500 font-semibold">Terms :</h5>
-                <p>{terms}</p>
-              </div>
-            </div>
-
-            <div className="flex justify-between space-x-10  mr-10">
-              <div className="space-y-2 text-gray-500">
-                <p>Subtotal</p>
-                <p>DisCount</p>
-                <p>Tax</p>
-                <p>Shipping</p>
-                <p>Total</p>
-                <p>Amount Paid</p>
-                <p>Balance Due</p>
-              </div>
-
-              <div className="space-y-2">
-                <p>{toCurrency(subtotal, "IDR")}</p>
-                <p>{toCurrency(discount, "IDR")}</p>
-                <p>{toCurrency(tax, "IDR")}</p>
-                <p>{toCurrency(payment, "IDR")}</p>
-                <p>{toCurrency(total, "IDR")}</p>
-                <p>{toCurrency(paid, "IDR")}</p>
-                <p>{toCurrency(Number(paid) - total, "IDR")}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
