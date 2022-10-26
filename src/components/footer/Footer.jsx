1647b4e785341c64134224c5791ac28c423222b4
DavidSitompul <dpaulsitompul@gmail.com>
2022-10-26 10:03:01 +0700

    perbaiki currency

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import Sidebar from "../footer/Sidebar"
 import Total from "../footer/Total"
 
-const Footer = ({ subtotal, notes, terms, setNotes, setTerms }) => {
+const Footer = ({ subtotal, notes, terms, setNotes, setTerms, toCurrency }) => {
   const [discount, setDiscount] = useState(0)
   const [payment, setPayment] = useState(0)
   const [tax, setTax] = useState(0)
@@ -22,13 +22,13 @@ const Footer = ({ subtotal, notes, terms, setNotes, setTerms }) => {
         </div>
 
         <div className="mt-2 mb-2 w-full">
-          <Total inputtext={"Subtotal"} value={subtotal} style={{ borderWidth: 0, boxShadow: "none", textAlign: "right" }} />
+          <Total inputtext={"Subtotal"} value={toCurrency(subtotal, "IDR")} style={{ borderWidth: 0, boxShadow: "none", textAlign: "right" }} />
           <Total inputtext={"Discount"} onChange={(e) => setDiscount(e.target.value)} value={discount} />
           <Total inputtext={"Tax"} onChange={(e) => setTax(e.target.value)} value={tax} />
           <Total inputtext={"shipping"} onChange={(e) => setPayment(e.target.value)} value={payment} />
-          <Total inputtext={"Total"} value={total} style={{ borderWidth: 0, boxShadow: "none", textAlign: "right" }} />
+          <Total inputtext={"Total"} value={toCurrency(total, "IDR")} style={{ borderWidth: 0, boxShadow: "none", textAlign: "right" }} />
           <Total inputtext={"Amount Paid"} onChange={(e) => setPaid(e.target.value)} value={paid} />
-          <Total inputtext={"Balance Due"} value={Number(paid) - total} style={{ borderWidth: 0, boxShadow: "none", textAlign: "right" }} />
+          <Total inputtext={"Balance Due"} value={toCurrency(Number(paid) - total, "IDR")} style={{ borderWidth: 0, boxShadow: "none", textAlign: "right" }} />
         </div>
       </div>
     </>
